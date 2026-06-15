192d15588051ab8eb9c0c81d16ebb8fe5f9499d4
keieffort2-coder <keieffort2@gmail.com>
2026-06-15 20:49:23 +0800

    Update upload-apimart-asset.js

--- api/upload-apimart-asset.js
+++ api/upload-apimart-asset.js
@@ -134,10 +134,16 @@ function findMessage(value, seen = new Set()) {
 }
 
 function sanitizeName(name) {
-  return String(name)
+  const cleaned = String(name)
+    .normalize("NFKD")
+    .replace(/[^\x20-\x7E]/g, "")
     .replace(/[/\\?%*:|"<>]/g, "-")
     .replace(/\s+/g, "-")
+    .replace(/[^A-Za-z0-9._-]/g, "-")
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "")
     .slice(0, 120);
+  return cleaned || "asset";
 }
 
 function getExtension(contentType) {
